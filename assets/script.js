11d9fe7b131def3f0bc50211ef6a7cd36b44ee54
Lainey Creighton <dev.lainey@gmail.com>
2023-06-29 00:59:35 -0700

    debug for loop

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,31 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
 //-----------------------------------PASSWORD CRITERIA VARIABLES-----------------------------------//
 //--------------------CHARACTERS--------------------//
 
 var typeNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var typeUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V"];
-var typeLower = ["a", "b", "c", "d", "e", "f", , "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var typeUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W","X","Y","Z"];
+var typeLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var typeSpecial = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "<", ">", "?", ":", ";", "~"];
 
 
 //--------------------EMPTY--------------------//
 
-var pwLength = 9;
 var charChosen = [];
+var pwLength = 128
 
 //-----------------------------------PASSWORD CRITERIA FUNCTION-----------------------------------//
 function pwCriteria() {
-
+  charChosen = [];
   //--------------------LENGTH--------------------//
-  var charLength = parseInt(prompt("Please provide desired password character length between 8 and 128:"));
-  if (charLength < 8 || charLength > 128 || (isNaN(charLength))) {
+  pwLength = parseInt(prompt("Please provide desired password character length between 8 and 128:"));
+  if (pwLength < 8 || pwLength > 128 || (isNaN(pwLength))) {
     alert("ALERT: Must be a number between 8 - 128 charaters!");
-
-    //-----------RETURN-----------//
+  //-----------RETURN-----------//
     return "Try Again!";
   }
-  confirm("You selected: " + charLength + " characters have been confirmed.");
+  confirm("You selected: " + pwLength + " characters have been confirmed.");
   alert("Answer the following prompts with 'OK' or 'Cancel' to select your character types.");
 
   //--------------------CHARACTERS--------------------//
@@ -47,21 +49,28 @@ function pwCriteria() {
   //-----------RETURN-----------//
   return true;
 }
+//-----------------------------------CREATE PASSWORD FUNCTION-----------------------------------//
+function generatePassword() {
+  var pwCreate = "";
+  for (var i = 0; i <= pwLength; i++) {
+    var sortChar = Math.floor(Math.random() * charChosen.length);
+    pwCreate += charChosen[sortChar];
+  }
 
-//-----------------------------------PASSWORD CRITERIA RETURN-----------------------------------//
+  //-----------RETURN-----------//
+  return pwCreate;
+}
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
+//-----------------------------------DISPLAY PASSWORD FUNCTION-----------------------------------//
 function writePassword() {
-  var returnCriteria = pwCriteria();
+  var displayCriteria = pwCriteria();
+  var passwordText = document.querySelector("#password");
 
-  if (returnCriteria) {
+  if (displayCriteria) {
     var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-
     passwordText.value = password;
+  } else {
+    alert("You MUST choose at least (1) character type!")
   }
 }
 
