0528db764b3624c987541981e7b1265500f1ac23
Lainey Creighton <dev.lainey@gmail.com>
2023-06-28 21:38:18 -0700

    add if statements to character criteria

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
 //-----------------------------------PASSWORD CRITERIA VARIABLES-----------------------------------//
 //--------------------CHARACTERS--------------------//
 
-var typeNumber = ["0","1","2","3","4","5","6","7","8","9"];
-var typeUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V"];
-var typeLower = ["a","b","c","d","e","f",,"g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var typeSpecial = ["!","@","#","$","%","^","&","*","(",")","-","_","=","+","<",">","?",":",";","~"];
+var typeNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var typeUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V"];
+var typeLower = ["a", "b", "c", "d", "e", "f", , "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var typeSpecial = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "<", ">", "?", ":", ";", "~"];
 
 
 //--------------------EMPTY--------------------//
@@ -14,23 +14,38 @@ var charChosen = [];
 
 //-----------------------------------PASSWORD CRITERIA FUNCTION-----------------------------------//
 function generatePassword() {
+  
   //--------------------LENGTH--------------------//
   var charLength = parseInt(prompt("Please provide desired password character length between 8 and 128:"));
   if (charLength < 8 || charLength > 128 || (isNaN(charLength))) {
     alert("ALERT: Must be a number between 8 - 128 charaters!");
+    
+    //-----------RETURN-----------//
     return "Try Again!";
   }
   confirm("You selected: " + charLength + " characters have been confirmed.");
   alert("Answer the following prompts with 'OK' or 'Cancel' to select your character types.");
+  
   //--------------------CHARACTERS--------------------//
-  var typeNumber = confirm("Would you like numbers?");
+  if (confirm("Would you like numbers?")) {
+    charChosen = charChosen.concat(typeNumber);
+  }
   alert("Numbers: " + typeNumber);
-  var typeUpper = confirm("Would you like upper case letters?");
+  if (confirm("Would you like upper case letters?")) {
+    charChosen = charChosen.concat(typeUpper);
+  }
   alert("Uppercase Letters: " + typeUpper);
-  var typeLower = confirm("Would you like lower case letters?");
+  if (confirm("Would you like lower case letters?")) {
+    charChosen = charChosen.concat(typeLower);
+  }
   alert("Lowercase Letters: " + typeLower);
-  var typeSpecial = confirm("Would you like special characters?");
+  if (confirm("Would you like special characters?")) {
+    charChosen = charChosen.concat(typeSpecial)
+  }
   alert("Special Characters: " + typeSpecial);
+
+  //-----------RETURN-----------//
+  return true;
 }
 
 //-----------------------------------PASSWORD CRITERIA RETURN-----------------------------------//
