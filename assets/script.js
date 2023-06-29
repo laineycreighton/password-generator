a9c5f9eb60202f801b4f06d24a9a1f0d4ad0a830
Lainey Creighton <dev.lainey@gmail.com>
2023-06-28 21:27:39 -0700

    add js comments and adjust prompts

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,51 @@
+//-----------------------------------PASSWORD CRITERIA VARIABLES-----------------------------------//
+//--------------------CHARACTERS--------------------//
+
+var typeNumber = ["0","1","2","3","4","5","6","7","8","9"];
+var typeUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V"];
+var typeLower = ["a","b","c","d","e","f",,"g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var typeSpecial = ["!","@","#","$","%","^","&","*","(",")","-","_","=","+","<",">","?",":",";","~"];
+
+
+//--------------------EMPTY--------------------//
+
+var pwLength = 9;
+var charChosen = [];
+
+//-----------------------------------PASSWORD CRITERIA FUNCTION-----------------------------------//
+function generatePassword() {
+  //--------------------LENGTH--------------------//
+  var charLength = parseInt(prompt("Please provide desired password character length between 8 and 128:"));
+  if (charLength < 8 || charLength > 128 || (isNaN(charLength))) {
+    alert("ALERT: Must be a number between 8 - 128 charaters!");
+    return "Try Again!";
+  }
+  confirm("You selected: " + charLength + " characters have been confirmed.");
+  alert("Answer the following prompts with 'OK' or 'Cancel' to select your character types.");
+  //--------------------CHARACTERS--------------------//
+  var typeNumber = confirm("Would you like numbers?");
+  alert("Numbers: " + typeNumber);
+  var typeUpper = confirm("Would you like upper case letters?");
+  alert("Uppercase Letters: " + typeUpper);
+  var typeLower = confirm("Would you like lower case letters?");
+  alert("Lowercase Letters: " + typeLower);
+  var typeSpecial = confirm("Would you like special characters?");
+  alert("Special Characters: " + typeSpecial);
+}
+
+//-----------------------------------PASSWORD CRITERIA RETURN-----------------------------------//
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function writePassword() {
-  var pwLength = prompt("Please provide desired password length:");
-  alert("You selected: " + pwLength + " characters.");
-  if (pwLength < 8 || pwLength > 128) {
-    alert("ALERT: Must be between 8 - 128 charaters!");
-  } else {
-    confirm(pwLength + " characters have been confirmed.");
-  }
-  alert("Answer the following prompts with 'Yes' or 'No' to select your character types.");
-  var typeNum = prompt("Would you like numbers?");
-  confirm("Numbers Included: " + typeNum);
-  var typeUpper = prompt("Would you like upper case letters?");
-  confirm("Upper Case Letters Included: " + typeUpper);
-  var typeLower = prompt("Would you like lower case letters?");
-  confirm("Lower Case Letters Included: " + typeLower);
-  var typeSpcChar = prompt("Would you like special characters?");
-  confirm("Special Characters Included: " + typeSpcChar);
-}
-
-
 // Write password to the #password input
-/*
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-*/
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
